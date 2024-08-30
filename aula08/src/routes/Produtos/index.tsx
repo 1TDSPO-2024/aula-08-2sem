d0394156a7b38dce0bdb1c3330b7e9f763b23ad2
SunaUezuri <wesley.jane.santos@gmail.com>
2024-08-29 21:44:10 -0300

    Criando uma lista com iteração

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,4 +1,4 @@
-
+import { listaProdutos } from "../../listaProdutos";
 
 export default function Produtos(){
 
@@ -7,7 +7,38 @@ export default function Produtos(){
 
     return(
       <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
+        <h1>Produtos Eletrônicos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Preço</th>
+              <th>Quantidade</th>
+              <th>Descrição</th>
+              <th>Imagem</th>
+              <th>Editar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaProdutos.map((produto) => (
+              <tr key={produto.id}>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.qtd}</td>
+                <td>{produto.descricao}</td>
+                <td><img src={produto.imagem} alt={produto.nome} /></td>
+                <td><button>Editar</button></td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={6}>
+                Quantidade de produtos: <span>{listaProdutos.length}</span>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
