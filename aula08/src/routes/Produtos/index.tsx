9e13075e9ddbcc692672426d18162086bbb7db61
GuiJanunzzi <guijanunzzi7@gmail.com>
2024-08-29 21:56:03 -0300

    Criado lista e instalado o react-icons

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,13 +1,43 @@
-
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../listaProdutos";
+import { BsBoxSeam as Editar} from "react-icons/bs";
 
 export default function Produtos(){
-
       //MUDANDO O TÍTULO DA PÁGINA!!!
       document.title = "PRODUTOS";
 
     return(
       <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
+        <h1>Produtos Eletrônicos</h1>
+        <table>
+          <thead>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Descrição</th>
+            <th>Imagem</th>
+            <th>Editar</th>
+          </thead>
+          <tbody>
+            {listaProdutos.map((produto)=>(
+              <tr key={produto.id}>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.qtd}</td>
+              <td>{produto.descricao}</td>
+              <td><img src={produto.imagem} alt={produto.nome} /></td>
+              <td><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link></td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={6}>
+                Quantidade de produtos: <span>{listaProdutos.length}</span>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
