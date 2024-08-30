e5d22552daa1740c969259173aec045dd4263555
BrennoVilela <brenno.cauet@gmail.com>
2024-08-29 21:43:07 -0300

    criando a lista e a tabela de produtos

---
+++ aula08/src/listaProdutos.ts
@@ -0,0 +1,16 @@
+type Lista = {
+    id: number;
+    nome: string;
+    preco: number;
+    qtd: number
+    descricao: string;
+    imagem: string;
+}
+
+export let listaProduto: Lista[] = [
+    { id: 1, nome: "Mouse", preco: 40.45, qtd: 10, descricao: "Mouse Optico", imagem: "mouse.jpg" },
+    { id: 2, nome: "Teclado Mecânico", preco: 489.00, qtd: 5, descricao: "Teclado Gamer", imagem: "teclado.jpg" },
+    { id: 3, nome: "Monitor Curvo", preco: 2.895, qtd: 3, descricao: "Montiro 32 polegadas", imagem: "monitor.jpg" },
+    { id: 4, nome: "Cadeira", preco: 2.589, qtd: 6, descricao: "Cadeira Gamer", imagem: "cadeira.jpg" },
+    { id: 5, nome: "Mouse pad", preco: 75.00, qtd: 32, descricao: "Cadeira pad temática", imagem: "mouse-pad.jpg" },
+]
--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,3 +1,4 @@
+import { listaProduto } from "../../listaProdutos";
 
 
 export default function Produtos(){
@@ -7,7 +8,39 @@ export default function Produtos(){
 
     return(
       <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
+        <h1>Produtos Eletrônicos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nome</th>
+              <th>Preço</th>
+              <th>Quantidade</th>
+              <th>Descrição</th>
+              <th>Imagem</th>
+              <th>Editar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaProduto.map((produto)=> (
+              <tr key={produto.id}>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.qtd}</td>
+                <td>{produto.descricao}</td>
+                <td><img src={produto.imagem} alt={produto.nome} /></td>
+                <td><button>Editar</button></td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={6}>
+                Quantidade de produtos: <span>{listaProduto.length}</span>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
